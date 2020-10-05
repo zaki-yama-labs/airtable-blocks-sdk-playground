6a6ef57ebeb6771d30e97c0e608adde5f74d9e1f
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-10-05 22:18:03 +0900

    To-do list tutorial (#1)
    * block init
    
    * Part 1
    
    * Part2 > Don't crash when there's no table
    
    * Part2 > Storing the selected table in state
    
    * Part2 > Using table ID instead of table name
    
    * Part3 > Storing configuration
    
    * Part3 > Permissions
    
    * Part4 > Tracking completed tasks
    
    * Part4 > Making it interactive
    
    * Extra credit > Only showing records from a view
    
    * add README

---
+++ todo_list/frontend/index.tsx
@@ -0,0 +1,100 @@
+import {
+  initializeBlock,
+  useBase,
+  useRecords,
+  expandRecord,
+  TextButton,
+  useGlobalConfig,
+  TablePickerSynced,
+  FieldPickerSynced,
+  ViewPickerSynced,
+} from "@airtable/blocks/ui";
+import React from "react";
+import Record from "@airtable/blocks/dist/types/src/models/record";
+
+function TodoApp() {
+  const base = useBase();
+
+  const globalConfig = useGlobalConfig();
+  const tableId = globalConfig.get("selectedTableId") as string;
+  const viewId = globalConfig.get("selectedViewId") as string;
+  const completedFieldId = globalConfig.get("completedFieldId") as string;
+
+  const table = base.getTableByIdIfExists(tableId);
+  const view = table ? table.getViewByIdIfExists(viewId) : null;
+  const completedField = table
+    ? table.getFieldByIdIfExists(completedFieldId)
+    : null;
+
+  const toggle = (record: Record) => {
+    table.updateRecordAsync(record, {
+      [completedFieldId]: !record.getCellValue(completedFieldId),
+    });
+  };
+
+  const records = useRecords(view);
+
+  const tasks =
+    records && completedField
+      ? records.map((record) => {
+          return (
+            <Task
+              key={record.id}
+              record={record}
+              onToggle={toggle}
+              completedFieldId={completedFieldId}
+            />
+          );
+        })
+      : null;
+  return (
+    <div>
+      <TablePickerSynced globalConfigKey="selectedTableId" />
+      <ViewPickerSynced table={table} globalConfigKey="selectedViewId" />
+      <FieldPickerSynced table={table} globalConfigKey="completedFieldId" />
+      {tasks}
+    </div>
+  );
+}
+
+type TaskProps = {
+  record: Record;
+  completedFieldId: string;
+  onToggle: (record: Record) => void;
+};
+
+function Task({ record, completedFieldId, onToggle }: TaskProps) {
+  const label = record.name || "Unnamed record";
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        fontSize: 18,
+        padding: 12,
+        borderBottom: "1px solid #ddd",
+      }}
+    >
+      <TextButton
+        variant="dark"
+        size="xlarge"
+        onClick={() => {
+          onToggle(record);
+        }}
+      >
+        {record.getCellValue(completedFieldId) ? <s>{label}</s> : label}
+      </TextButton>
+      <TextButton
+        icon="expand"
+        aria-label="Expand record"
+        variant="dark"
+        onClick={() => {
+          expandRecord(record);
+        }}
+      />
+    </div>
+  );
+}
+
+initializeBlock(() => <TodoApp />);
