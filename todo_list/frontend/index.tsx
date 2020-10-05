620132ac7c1c4b7e6a7a6bba7ec6f372bf7f4132
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-10-05 21:58:03 +0900

    Part4 > Tracking completed tasks

--- todo_list/frontend/index.tsx
+++ todo_list/frontend/index.tsx
@@ -6,32 +6,53 @@ import {
   TextButton,
   useGlobalConfig,
   TablePickerSynced,
+  FieldPickerSynced,
 } from "@airtable/blocks/ui";
-import React, { useState } from "react";
+import React from "react";
+import Record from "@airtable/blocks/dist/types/src/models/record";
 
 function TodoApp() {
   const base = useBase();
 
   const globalConfig = useGlobalConfig();
   const tableId = globalConfig.get("selectedTableId") as string;
+  const completedFieldId = globalConfig.get("completedFieldId") as string;
+
   const table = base.getTableByIdIfExists(tableId);
+  const completedField = table
+    ? table.getFieldByIdIfExists(completedFieldId)
+    : null;
 
   const records = useRecords(table);
 
-  const tasks = records
-    ? records.map((record) => {
-        return <Task key={record.id} record={record} />;
-      })
-    : null;
+  const tasks =
+    records && completedField
+      ? records.map((record) => {
+          return (
+            <Task
+              key={record.id}
+              record={record}
+              completedFieldId={completedFieldId}
+            />
+          );
+        })
+      : null;
   return (
     <div>
       <TablePickerSynced globalConfigKey="selectedTableId" />
+      <FieldPickerSynced table={table} globalConfigKey="completedFieldId" />
       {tasks}
     </div>
   );
 }
 
-function Task({ record }) {
+type TaskProps = {
+  record: Record;
+  completedFieldId: string;
+};
+
+function Task({ record, completedFieldId }: TaskProps) {
+  const label = record.name || "Unnamed record";
   return (
     <div
       style={{
@@ -43,7 +64,7 @@ function Task({ record }) {
         borderBottom: "1px solid #ddd",
       }}
     >
-      {record.name || "Unnamed record"}
+      {record.getCellValue(completedFieldId) ? <s>{label}</s> : label}
       <TextButton
         icon="expand"
         aria-label="Expand record"
